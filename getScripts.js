e0e6bfc31c7b9e76dde97afba0dc639e32eea81b
Martijn Geerts <martijn.geerts@gmail.com>
2018-01-28 10:48:57 +0100

    Update getScripts.js

--- getScripts.js
+++ getScripts.js
@@ -19,34 +19,36 @@
 			failed: []
 		};
 
-		o.getFilename = function (filename) {
+		var f = {};
+
+		f.getFilename = function (filename) {
 			var a = document.createElement("a");
 			a.href = filename;
 			return a.href.split("?")[0];
 		};
 
-		o.triggerDone = function () {
+		f.triggerDone = function () {
 			o.file = this.url === undefined ? o.fail : this.url;
-			o.file = o.getFilename(o.file);
+			o.file = f.getFilename(o.file);
 			if (++o.i === o.loaded.length) {
 				o.el.css(o.css);
 				o.el.trigger("ready", [o.failed]);
 			}
 		};
 
-		o.triggerFail = function () {
-			o.fail = o.getFilename(this.url);
+		f.triggerFail = function () {
+			o.fail = f.getFilename(this.url);
 			o.failed.push(o.fail);
-			o.triggerDone();
+			f.triggerDone();
 		};
 
 		window.config = window.config || {};
 		window.config.gotScripts = window.config.gotScripts || [];
 
 		$.each(scripts, function () {
-			o.file = o.getFilename(this);
+			o.file = f.getFilename(this);
 			if (window.config.gotScripts.indexOf(o.file) === -1) {
-				$.getScript(o.file).done(o.triggerDone).fail(o.triggerFail);
+				$.getScript(o.file).done(f.triggerDone).fail(f.triggerFail);
 				window.config.gotScripts.push(o.file);
 				o.loaded.push(o.file);
 			}
